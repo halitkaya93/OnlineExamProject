27d46458612865b8c8dfa20a7d0634d3adb11e37
halitkaya93 <66157683+halitkaya93@users.noreply.github.com>
2020-06-11 00:52:22 +0300

    Update DBEXAMContext.cs

--- OnlineExamProject/DataDb/DBEXAMContext.cs
+++ OnlineExamProject/DataDb/DBEXAMContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
@@ -25,12 +25,12 @@ namespace OnlineExamProject.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=HKAYA93;Database=DBEXAM;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=HKAYA93;Database=master;Trusted_Connection=True;");
             }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
+        { 
             modelBuilder.Entity<Questions>(entity =>
             {
                 entity.HasKey(e => e.QuestionId)
